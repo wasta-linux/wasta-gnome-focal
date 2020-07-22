4c70a8be5b8ee1b8ab4323f7761628625fe30386
Nate Marti <nate_marti@sil.org>
2020-07-22 16:24:06 +0200

    Initial commit.

---
+++ wasta-gnome/wasta-gnome-postinst.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# ==============================================================================
+# wasta-gnome-postinst.sh
+#
+#   This script is automatically run by the postinst configure step on
+#       installation of wasta-cinnamon-*. It can be manually re-run, but
+#       is only intended to be run at package installation.
+#
+#   2020-07-22 ndm: initial script
+#
+# ==============================================================================
+
+# ------------------------------------------------------------------------------
+# Check to ensure running as root
+# ------------------------------------------------------------------------------
+#   No fancy "double click" here because normal user should never need to run
+if [ $(id -u) -ne 0 ]
+then
+	echo
+	echo "You must run this script with sudo." >&2
+	echo "Exiting...."
+	sleep 5s
+	exit 1
+fi
+
+# ------------------------------------------------------------------------------
+# Initial setup
+# ------------------------------------------------------------------------------
+
+echo
+echo "*** Script Entry: wasta-gnome-postinst.sh"
+echo
+
+# Setup Directory for later reference
+DIR=/usr/share/wasta-gnome
+
+# ------------------------------------------------------------------------------
+# Dconf / Gsettings Default Value adjustments
+# ------------------------------------------------------------------------------
+echo
+echo "*** Updating dconf / gsettings default values"
+echo
+
+# Updating dconf first to incorporate those direct entries first.
+dconf update
+
+# MAIN System schemas: we have placed our override file in this directory
+# Sending any "error" to null (if key not found don't want to worry user)
+glib-compile-schemas /usr/share/glib-2.0/schemas/ > /dev/null 2>&1 || true;
+
+# ------------------------------------------------------------------------------
+# Finished
+# ------------------------------------------------------------------------------
+echo
+echo "*** Script Exit: wasta-gnome-postinst.sh"
+echo
+
+exit 0
