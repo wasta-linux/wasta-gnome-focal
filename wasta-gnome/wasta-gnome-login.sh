405a57df0a5d619296570ac55c72777775375d41
Nate Marti <nate_marti@sil.org>
2020-12-03 11:36:02 +0100

    added error log

--- wasta-gnome/wasta-gnome-login.sh
+++ wasta-gnome/wasta-gnome-login.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+LOG="/var/log/wasta-multidesktop/wasta-gnome-login-errors"
+date | tee -a "${LOG}"
+
 # Get current user and session name (can't depend on env at login).
 CURR_USER=$(grep -a "User .* authorized" /var/log/lightdm/lightdm.log | \
     tail -1 | sed 's@.*User \(.*\) authorized@\1@')
@@ -15,8 +18,8 @@ fi
 if [[ -e /usr/share/dbus-1/services/org.gnome.ScreenSaver.service.disabled ]]; then
     mv /usr/share/dbus-1/services/org.gnome.ScreenSaver.service{.disabled,}
 else
-    # gnome-screensaver not properly installed for some reason.
-    echo "gnome-screensaver not properly installed"
+    # gnome-screensaver not previously disabled at login.
+    echo "gnome-screensaver not disabled prior to login" | tee -a "${LOG}"
 fi
 
 # Reset ...app-folders folder-children if it's currently set as ['Utilities', 'YaST']
@@ -24,5 +27,6 @@ key_path='org.gnome.desktop.app-folders'
 key='folder-children'
 curr_children=$(sudo --user=$CURR_USER gsettings get "$key_path" "$key")
 if [[ $curr_children = "['Utilities', 'YaST']" ]]; then
-    sudo --user=$CURR_USER --set-home dbus-launch gsettings reset "$key_path" "$key"
+    sudo --user=$CURR_USER --set-home dbus-launch gsettings reset "$key_path" "$key" 2>&1 >/dev/null | tee -a "${LOG}"
 fi
+echo | tee -a "${LOG}"
