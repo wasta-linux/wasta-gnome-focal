d19351d2c2331040ffffd5b6903dbe8761b60563
Nate Marti <nate_marti@sil.org>
2021-04-08 16:18:46 +0100

    don't reconfigure lightdm

--- wasta-gnome/wasta-gnome-postinst.sh
+++ wasta-gnome/wasta-gnome-postinst.sh
@@ -46,12 +46,12 @@ DIR=/usr/share/wasta-gnome
 #    | debconf-set-selections
 
 # Determine current display manager.
-dm=$(systemctl status display-manager.service | grep 'Main PID:' | awk -F'(' '{print $2}')
+#dm=$(systemctl status display-manager.service | grep 'Main PID:' | awk -F'(' '{print $2}')
 # Get rid of 2nd parenthesis.
-dm="${dm::-1}"
-if [[ $dm == 'lightdm' ]]; then
-	dpkg-reconfigure lightdm
-fi
+#dm="${dm::-1}"
+#if [[ $dm == 'lightdm' ]]; then
+#	dpkg-reconfigure lightdm
+#fi
 
 # Set GDM3 default config.
 if [[ -e /etc/gdm3 ]]; then
