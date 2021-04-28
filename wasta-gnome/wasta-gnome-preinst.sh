e7d8879b30a539e1043918c122a786d9191cf1dd
Nate Marti <nate_marti@sil.org>
2021-04-28 13:49:23 +0100

    abandon gdm3 preseed idea

--- wasta-gnome/wasta-gnome-preinst.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Auto-select current display-manager to keep.
-DM=$(systemctl show display-manager.service | grep Id= | awk -F= '{print $2}')
-echo "gdm3	shared/default-x-display-manager	select	$DM" |\
-	debconf-set-selections
-echo "gdm3	shared/default-x-display-manager seen" |\
-	debconf-set-selections
